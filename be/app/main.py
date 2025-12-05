7628fae537117db9f8027f4340976f3ce69eb512
winno <rafiwinno13@gmail.com>
2025-12-05 14:48:32 +0700

    Update main.py

--- be/app/main.py
+++ be/app/main.py
@@ -1,27 +1,36 @@
-from fastapi import FastAPI, HTTPException
-
-from app.schemas import RecommendationRequest, RecommendationResponse
+# app/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.services.ml_services import predict_recommendation
+from app.schemas import RecommendationRequest, RecommendationResponse
 
 app = FastAPI(
     title="Telco Product Recommendation API",
     version="1.0.0",
-    description="Backend untuk capstone Asah – rekomendasi produk telco berbasis XGBoost.",
+)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello from Winno Backend with XGBoost pipeline"}
+    return {"message": "Hello World from Backend with XGBoost!"}
 
 
 @app.post("/api/v1/recommend", response_model=RecommendationResponse)
 def recommend(req: RecommendationRequest):
-    try:
-        label, conf = predict_recommendation(req.dict())
-        return RecommendationResponse(
-            recommended_offer=label,
-            confidence=conf,
-        )
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    label, conf = predict_recommendation(req.dict())
+    return {"recommended_offer": label, "confidence": conf}
